c37cb8bb84bc88f1e3e45307af84ca519a075d99
Simon <152021717+SimonR-prog@users.noreply.github.com>
2023-11-26 15:31:33 +0100

    upg 3.3 klar
    upg 3.3 klar

--- Uppgift 3/Prog2_Simon_R_Uppgift_3_Övningsuppgifter.py
+++ Uppgift 3/Prog2_Simon_R_Uppgift_3_Övningsuppgifter.py
@@ -29,7 +29,7 @@ Output;
 How many hours of sleep needed, depending on age. """
 
 #Using dict with age for key and hours of sleep as value:
-sleep = {1:14, 2:13, 3:12, 4:11.5, 5 and 6:11,7:10.5, 8 and 9 and 10:10, 11:9.5, 12 and 13 and 14 and 15:9, 16:8.5}
+sleep = {1:14, 2:13, 3:12, 4:11.5, 5 and 6:11, 7:10.5, 8 and 9 and 10:10, 11:9.5, 12 and 13 and 14 and 15:9, 16:8.5}
 try:
     name = str(input("What is your name? > "))
     age = int(input("What is your age? > "))
@@ -44,8 +44,51 @@ except:
 
 #3.3;
 
-
-
+import random
+dice_width = 11
+while True:
+    dice = random.randint(1, 6)
+    roll = input('Type "roll" to roll the dice and "end" to end. > ').lower()
+    if roll == "roll":
+        print(f"You rolled a {dice}!")
+        if dice == 1:
+            print(dice_width * "-")
+            print("|", " " * 7,"|")
+            print("|"," "*2,"X"," "*2,"|")
+            print("|", " " * 7, "|")
+            print(dice_width * "-")
+        elif dice == 2:
+            print(dice_width * "-")
+            print("|", "X", " " * 5, "|")
+            print("|", " " * 7, "|")
+            print("|", " " * 5, "X", "|")
+            print(dice_width * "-")
+        elif dice == 3:
+            print(dice_width * "-")
+            print("|", "X", " " * 5, "|")
+            print("|"," "*2,"X"," "*2,"|")
+            print("|", " " * 5, "X", "|")
+            print(dice_width * "-")
+        elif dice == 4:
+            print(dice_width * "-")
+            print("|","X", " " * 3, "X", "|")
+            print("|", " " * 7,"|")
+            print("|","X", " " * 3, "X", "|")
+            print(dice_width * "-")
+        elif dice == 5:
+            print(dice_width * "-")
+            print("|", "X", " " * 3, "X", "|")
+            print("|", " " * 2, "X", " " * 2, "|")
+            print("|", "X", " " * 3, "X", "|")
+            print(dice_width * "-")
+        else:
+            print(dice_width * "-")
+            print("|", "X", " " * 3, "X", "|")
+            print("|", "X", " " * 3, "X", "|")
+            print("|", "X", " " * 3, "X", "|")
+            print(dice_width * "-")
+    elif roll == "end":
+        break
 
 
 
