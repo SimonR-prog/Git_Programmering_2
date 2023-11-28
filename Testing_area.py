fa977d5c376ef0ca543ba0cf1b8aa99c01fc9537
Simon <152021717+SimonR-prog@users.noreply.github.com>
2023-11-28 23:12:22 +0100

    Update Testing_area.py

--- Testing_area.py
+++ Testing_area.py
@@ -24,18 +24,52 @@ def print_list(): # Function to print the to do list..
     print("*" * 15)
     print("To do list;".center(15))
     for i in todo:
+
         # Convert the int to string to concatenate.
         # Adding number before each to use later for removing indexes.
+
         print(str(list_index) + ". " + i)
         list_index += 1
 
 
-def remove_thing():
-    choice = input("What would you like to remove? > ")
+def remove_todo():
+    choice = input("What would you like to remove? \n(Can type the thing or use the number next to it.) \n Choose; > ").lower()
+
+def add_todo():
+    choice = input("What would you like to add to your to do list? > ")
+    # Need to check so that the string is not a number or whatever..
+    if choice in todo:
+        print(f"{choice} is already in the list.")
 
 while True:
     # Starting list;
     todo = ["Städa", "Handla", "Plugga", "Ge blod"]
     clear_console()
     print_list()
-    break
+    print("*"*15)
+    choice = input("  Command; \n1. Add thing to do. \n2. Remove a thing from to do. \n4.Close. \nChoose; > ")
+    if choice == "1":
+        add_todo()
+    elif choice == "2":
+        remove_todo()
+    elif choice == "3":
+        continue
+    elif choice == "4":
+        break
+    else:
+        print("Invalid input.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
