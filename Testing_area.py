060704f72d1c6aed18b0d367382e3357c739d4c7
Simon <152021717+SimonR-prog@users.noreply.github.com>
2023-11-28 22:30:05 +0100

    update
    update

--- Testing_area.py
+++ Testing_area.py
@@ -1,8 +1,41 @@
+# Creating a program for a to do with while loop for fun;
+"""
+Clear console everytime it runs.
+Show the list.
+    Print it with a for loop to add numbers to all things for easy removal with index.
+Allow person to remove things by index or name of thing.
+    When using index, remove 1 from the number they input to get the index.
+    Ask them for a y/n before removing.
+Allow person to add things to the list.
+Allow person to check if something is on the list.
+"""
 
-y = range(1,100)
-t = int(input("num > "))
+import os
 
-if t in y:
-    print("yes")
-else:
-    print("no")
+def clear_console(): # Function to clear the console.
+    if os.name == 'nt':
+        os.system('cls')
+    elif os.name == 'posix':
+        os.system('clear')
+
+
+def print_list(): # Function to print the to do list..
+    list_index = 1
+    print("*" * 15)
+    print("To do list;".center(15))
+    for i in todo:
+        # Convert the int to string to concatenate.
+        # Adding number before each to use later for removing indexes.
+        print(str(list_index) + ". " + i)
+        list_index += 1
+
+
+def remove_thing():
+    choice = input("What would you like to remove? > ")
+
+while True:
+    # Starting list;
+    todo = ["Städa", "Handla", "Plugga", "Ge blod"]
+    clear_console()
+    print_list()
+    break
--- Uppgift 4/Prog2_Simon_R_Uppgift_4.py
+++ Uppgift 4/Prog2_Simon_R_Uppgift_4.py
@@ -1,42 +1,35 @@
+# https://christianaberg.files.wordpress.com/2020/07/dva128___kompendium.pdf
+# 8.2
 """Define;
-
-1. Print out the first three of the things in the list. 
-Guessing it means by index.
-
+Assignment wants us to do several things with the same to do list;
+1. Print out the first three of the things in the list.
 2. Print out the list. Append the list with "Fetch dog..". Print it out again.
-
 3. Allow user to remove a thing of the list by inputting an index.
 Print the list before and after to confirm.
 Need list.pop(index)
-
 4. Allow user to remove a thing via value input. 
 If x in list yadayada.. Use remove(element).
-
 5. Allow user to add something to the list and then sort it. Print before and 
 after. string.sort()
-
 6. Through input, user can check if something is in list. If x in list = response
 if x not in list = respone + ask if user wants to add it. Print list yadayada..
 """
-
-#8.2
-
-#8.1;
+# 8.1;
 todo = ["Städa","Handla","Plugga","Ge blod"]
 print(todo[:3])
-#or
+# or
 print(todo[0])
 print(todo[1])
 print(todo[2])
 
-#8.2;
+# 8.2;
 todo = ["Städa","Handla","Plugga","Ge blod"]
 print(todo)
 add = input("What would you like to add to the to do? > ").capitalize()
 todo.append(add)
 print(todo)
 
-#8.3;
+# 8.3;
 todo = ["Städa","Handla","Plugga","Ge blod"]
 print(todo)
 try:
@@ -46,7 +39,7 @@ except:
     print("Must input a whole number for which index you want to remove.")
 print(todo)
 
-#8.4;
+# 8.4;
 todo = ["Städa","Handla","Plugga","Ge blod"]
 print(todo)
 rem = input("Which one would you like to remove? > ").capitalize()
@@ -57,7 +50,7 @@ else:
     print(f"{rem} is not in to do.")
 print(todo)
 
-#8.5;
+# 8.5;
 todo = ["Städa","Handla","Plugga","Ge blod"]
 print(todo)
 add = input("What would you like to add to the list? > ").capitalize()
@@ -66,7 +59,7 @@ print(f"{add} has been added.")
 todo.sort()
 print(todo)
 
-#8.6;
+# 8.6;
 todo = ["Städa","Handla","Plugga","Ge blod"]
 print(todo)
 add = input("What to do? > ").capitalize()
@@ -81,6 +74,35 @@ else:
     print(f"{add} is in the to do.")
 todo.sort()
 print(todo)
+
+
+
+
+# Creating a program for a to do with while loop for fun;
+"""
+Clear console everytime. 
+Show the list. 
+    Print it with a for loop to add numbers to all things for easy removal with index.
+Allow person to remove things by index or name of thing.
+    When using index, remove 1 from the number they input to get the index.
+    Ask them for a y/n before removing.
+Allow person to add things to the list.
+Allow person to check if something is on the list.
+"""
+import os
+# Starting list;
+todo = ["Städa","Handla","Plugga","Ge blod"]
+while True:
+    # Clear os;
+    if os.name == 'nt':
+        os.system('cls')
+    elif os.name == 'posix':
+        os.system('clear')
+    for i in todo:
+        print(todo[i]," ", i)
+
+
+
 
 
 
