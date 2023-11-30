c3f5c94e4d04c0ac86aaa38d54e48dfe5d24ee89
Simon <152021717+SimonR-prog@users.noreply.github.com>
2023-11-30 23:18:30 +0100

    update
    update

--- Uppgift 4/Prog2_Simon_R_Uppgift_4.py
+++ Uppgift 4/Prog2_Simon_R_Uppgift_4.py
@@ -14,13 +14,24 @@ after. string.sort()
 6. Through input, user can check if something is in list. If x in list = response
 if x not in list = respone + ask if user wants to add it. Print list yadayada..
 """
+rad = "-------------------------------" # Spacer for console.
 # 8.1;
 todo = ["Städa","Handla","Plugga","Ge blod"]
-print(todo[:3])
+print(todo[:3]) # Using index. Printing up to 3 but wont print the 3d index.
+print(rad)
+
 # or
 print(todo[0])
 print(todo[1])
 print(todo[2])
+print(rad)
+
+# or
+i = 0
+while i < 3:
+    print(todo[i])
+    i += 1
+print(rad)
 
 # 8.2;
 todo = ["Städa","Handla","Plugga","Ge blod"]
@@ -28,6 +39,7 @@ print(todo)
 add = input("What would you like to add to the to do? > ").capitalize()
 todo.append(add)
 print(todo)
+print(rad)
 
 # 8.3;
 todo = ["Städa","Handla","Plugga","Ge blod"]
@@ -38,6 +50,7 @@ try:
 except:
     print("Must input a whole number for which index you want to remove.")
 print(todo)
+print(rad)
 
 # 8.4;
 todo = ["Städa","Handla","Plugga","Ge blod"]
@@ -49,6 +62,7 @@ if rem in todo:
 else:
     print(f"{rem} is not in to do.")
 print(todo)
+print(rad)
 
 # 8.5;
 todo = ["Städa","Handla","Plugga","Ge blod"]
@@ -58,63 +72,20 @@ todo.append(add)
 print(f"{add} has been added.")
 todo.sort()
 print(todo)
+print(rad)
 
 # 8.6;
 todo = ["Städa","Handla","Plugga","Ge blod"]
 print(todo)
-add = input("What to do? > ").capitalize()
-if add not in todo:
-    choice = input(f"{add} is not on the list, would you like to add it? Y/N > ").upper()
+check = input("What do you want to check? > ").capitalize()
+if check not in todo:
+    choice = input(f"{check} is not on the list, would you like to add it? Y/N > ").upper()
     if choice == "Y" or choice == "YES":
-        todo.append(add)
-        print(f"{add} has been added to the to do.")
+        todo.append(check)
+        print(f"{check} has been added to the to do.")
     else:
         print("Okey.")
 else:
-    print(f"{add} is in the to do.")
+    print(f"{check} is in the to do.")
 todo.sort()
 print(todo)
-
-
-
-
-# Creating a program for a to do with while loop for fun;
-"""
-Clear console everytime. 
-Show the list. 
-    Print it with a for loop to add numbers to all things for easy removal with index.
-Allow person to remove things by index or name of thing.
-    When using index, remove 1 from the number they input to get the index.
-    Ask them for a y/n before removing.
-Allow person to add things to the list.
-Allow person to check if something is on the list.
-"""
-import os
-# Starting list;
-todo = ["Städa","Handla","Plugga","Ge blod"]
-while True:
-    # Clear os;
-    if os.name == 'nt':
-        os.system('cls')
-    elif os.name == 'posix':
-        os.system('clear')
-    for i in todo:
-        print(todo[i]," ", i)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
