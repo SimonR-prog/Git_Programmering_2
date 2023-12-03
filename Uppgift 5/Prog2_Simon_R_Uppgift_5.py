e111e2f911658fa736a8f0d003dafcfa5eb4fc48
Simon <152021717+SimonR-prog@users.noreply.github.com>
2023-12-04 00:04:33 +0100

    upd
    upd

--- Uppgift 5/Prog2_Simon_R_Uppgift_5.py
+++ Uppgift 5/Prog2_Simon_R_Uppgift_5.py
@@ -1,13 +1,58 @@
 # 11.3:
 
-"""
-Defining program;
-Create a program that takes several int inputs, puts them in a file and
+import json
+import os
 
+def clear_console(): # Function to clear the console.
+    if os.name == 'nt':
+        os.system('cls')
+    elif os.name == 'posix':
+        os.system('clear')
 
+def counter(): # Function to sum up everything in the file and send sum and list to menu.
+    with open('memorizer.json') as f:
+        int_list = f.read()
+        int_list = json.loads(int_list)
+    sum = 0
+    for i in int_list:
+        sum += int(i)
+    menu(int_list,sum)
 
-Output;
-A menu that prints all the previous numbers in a list to show the person.
-The total of all the numbers involved.
+def menu(int_list,sum): # Function to print the menu, previous numbers, the sum and the instructions.
+    print("*"*20)
+    print("Intmemorizer;".center(20))
+    print("*"*20)
+    for i in int_list:
+        print("*",i)
+    print("-"*20)
+    print("Summa;", sum)
+    print("-" * 20)
+    print("Input whole numbers.")
+    print('Typing "0" will close program.')
+    print("-"*20)
 
-"""
+def export(number): # Function to add the new number to the file.
+    with open('memorizer.json') as f:
+        int_list = f.read()
+        int_list = json.loads(int_list)
+    if number not in int_list:
+        int_list.append(number)
+        with open('memorizer.json', 'w') as f:
+            f.write(json.dumps(int_list))
+
+while True:
+    # Clearing console every time loop starts;
+    clear_console()
+    # Starts the counter function;
+    counter()
+    # Try/except to catch ValueErrors.
+    try:
+        number = int(input("> "))
+        if number != 0: # If number isn't a 0 it gets sent on to export function.
+            export(number)
+        else:
+            print("Closing.")
+            break
+    except ValueError:
+        print("ValueError. Must type a number.")
+        enter = input("Press enter to continue.")
