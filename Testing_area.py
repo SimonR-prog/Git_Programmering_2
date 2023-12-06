f5a503fe61d89338c644b1bbad2219a314d1dbe2
Simon <152021717+SimonR-prog@users.noreply.github.com>
2023-12-06 21:24:09 +0100

    upd
    upd

--- Testing_area.py
+++ Testing_area.py
@@ -1,29 +1,12 @@
-import os
-import json
-
-def clear_console(): # Function to clear the console.
-    if os.name == 'nt':
-        os.system('cls')
-    elif os.name == 'posix':
-        os.system('clear')
-
-def menu():
-    with open('notepad.json') as f:
-        notes_dict = f.read()
-        notes_dict = json.loads(notes_dict)
-        return notes_dict
-
-def printer(): # to print the keys.
-    notes_dict = menu()
-    for i in notes_dict:
-        print(i)
-
-def printer_2(): # To print the value.
-    notes_dict = menu()
-    for i in notes_dict:
-        print(notes_dict[i][0])
 
 
-printer()
-printer_2()
+
+
+
+
+
+
+
+
+
 
--- Uppgift 6/Prog2_Simon_R_Uppgift_6.py
+++ Uppgift 6/Prog2_Simon_R_Uppgift_6.py
@@ -2,25 +2,6 @@
 
 # 12.6:
 
-""" Defining in no order;
-A menu with instructions.
-A dictionary with a titel as key and text as value.
-User ability to list, show, create, change and remove items from dictionary.
-Clear console.
-Import and export to a separate file to save between runs.
-
-Want to change it so that the person can type in a number to call for a certain key?
-
-Output;
-Clear console.
-Print menu with the keys and instructions.
-
-
-Print the value of the first thing in dict;
-print(notes_dict["1"])
-
-
-"""
 import os
 import json
 
@@ -30,23 +11,30 @@ def clear_console(): # Function to clear the console.
     elif os.name == 'posix':
         os.system('clear')
 
+def importer(): # Function to import the dictionary and return it.
+    with open('notepad.json') as f:
+        notes_dict = f.read()
+        notes_dict = json.loads(notes_dict)
+        return notes_dict
+
 def exporter(notes_dict): # Function to export the dictionary again after modifying it.
-
-    notes_dict["Ny"] = "Nyast"
-    # import through other function.
-
     with open('notepad.json', 'w') as f:
         f.write(json.dumps(notes_dict))
 
-def changer(decide, key): # Function to remove or add a note.
+def changer(choice): # Function to remove, add or change a note.
     notes_dict = importer() # Fetches the dictionary.
-    if decide == "remove":
-        if key.capitalize() in notes_dict: # Check for it's existence
-            notes_dict.pop(key.capitalize())
-            exporter(notes_dict)
+    if choice == "remove":
+        print("Which note would you like to remove?")
+        key = input("> ").capitalize()
+        if key in notes_dict: # Check for it's existence
+            print(f"{key} - {notes_dict[key]} has been removed.")
+            del notes_dict[key]
+            exporter(notes_dict)  # Updates the dict after change has been made.
         else:
             print("There is no such note.")
-    else:
+    elif choice == "add":
+        print("What title do you want for the new note?")
+        key = input("> ")
         if key.capitalize() not in notes_dict: # If key doesn't already exist.
             print("What text do you want to add to the note?")
             value = input("> ")
@@ -54,68 +42,69 @@ def changer(decide, key): # Function to remove or add a note.
             exporter(notes_dict)
         else:
             print("That note already exists.")
-
-def importer():
-    with open('notepad.json') as f:
-        notes_dict = f.read()
-        notes_dict = json.loads(notes_dict)
-        return notes_dict
-
-def viewer(view): # Function to print the value of the key the person chooses.
-    notes_dict = importer()
-    if len(notes_dict) > 0:
-        if view in notes_dict:
-            print(notes_dict[view])
+    elif choice == "change":
+        print("Which one would you like to change?")
+        key = input("> ").capitalize()
+        if key in notes_dict:
+            print(f"Current text is; {notes_dict[key]}")
+            print("What would you like to change it to?")
+            new_value = input("> ").capitalize()
+            notes_dict[key] = new_value
+            exporter(notes_dict)
         else:
             print("There's no such note.")
+
+def viewer(): # Function to print the value of the key the person chooses.
+    notes_dict = importer()
+    if len(notes_dict) > 0:
+        print("Would you like to view all or a specific note? (All/Spec)")
+        choice = input("> ").lower()
+        if choice == "all":
+            print("-"*20)
+            for i in notes_dict:
+                print(i + ";\n-" + notes_dict[i])
+        elif choice == "spec":
+            print("Which note would you like to view?")
+            view = input("> ").capitalize()
+            if view in notes_dict:
+                print("-" * 20)
+                print(notes_dict[view])
+            else:
+                print("There's no such note.")
+        else:
+            print('You have to type either "all" or "spec".')
     else:
         print("The file is empty.")
-def printer_2(): # To print the value.
-    notes_dict = importer()
-    for i in notes_dict:
-        print(notes_dict[i][0])
 
-
-def menu():
+def menu(): # Function to print the menu, the list of keys and the instructions.
     notes_dict = importer()
     print("*"*20)
-    print("*"*5,"Notes".center(8),"*"*5)
+    print("Notes".center(20))
     print("*"*20)
     for i in notes_dict:
         print(i)
     print("*"*20)
-    instructions = ["View | View note", "Add | Add note","RM | Remove note","Exit | End program"]
+    instructions = {"View":"View note", "Add":"Add note","Remove":"Remove note","Change":"Change note text","Exit":"End program"}
+    print("Instructions".center(20))
+    print("*"*20)
     for i in instructions:
-        print(i)
+        print(i + " - " + instructions[i])
+    print("*"*20)
 
 while True:
     clear_console()
     menu()
+    menu_choice = ["view","add","remove","change","show","exit"]
     choice = input("> ").lower()
-    if choice in ["view","add","rm","exit"]:
+    if choice in menu_choice:
         if choice == "view":
-            print("Which would you like to view?")
-            view = input("> ")
-            viewer(view)
-        elif choice == "add":
-            print("What would you like to add?")
-            key = input("> ")
-            decide = "add"
-            changer(decide, key)
-        elif choice == "rm":
-            print("Which would you like to remove?")
-            key = input("> ")
-            decide = "remove"
-            changer(decide, key)
+            viewer()
+        elif choice in ["add","remove","change"]:
+            changer(choice)
         elif choice == "exit":
             print("Ending program.")
             break
+    else:
+        print("Must choose from the list of instructions.")
+    print("-" * 20)
     enter = input("Press enter to continue.")
-
-"""Want to use the exporter function from adding or removing functions. 
-Send a string with either remove or add and then a 
-variable to the export function and do an if else.
-
-Change the add turn the thing into a dict variable to later add. 
-
-"""
