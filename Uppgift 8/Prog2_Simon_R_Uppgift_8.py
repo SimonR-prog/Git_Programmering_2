f6ff3285b17b1a622ee1343ef0a250a0fb91f4b4
Simon <152021717+SimonR-prog@users.noreply.github.com>
2024-01-06 20:31:57 +0100

    Update Prog2_Simon_R_Uppgift_8.py

--- Uppgift 8/Prog2_Simon_R_Uppgift_8.py
+++ Uppgift 8/Prog2_Simon_R_Uppgift_8.py
@@ -58,19 +58,20 @@ def make_list(dict): # Takes the previous dictionary and makes it into a list. A
     teams = list
     return teams
 
+# 15.3:
+
 def sort_list(list):
     for i in list:
         points = (int(i["wins"]) * 3) + int(i["draws"])
         i["points"] = points
-    teams_list_sorted = sorted(list, key=lambda x: x["points"], reverse=True)
+    teams_list_sorted = sorted(list, key=lambda dict: dict["points"], reverse=True)
     return teams_list_sorted
-
 
 # 14.6:
 def print_table(list):
     num = 1
     print("-"*40)
-    print("| # | Nation | W | D | L | GF | GA | GD | P |" )
+    print("| # |", " Nation".ljust(14), "| W | D | L | GF | GA | GD | P |" )
     print("-" * 30)
     for i in list:
         nation = i["country"]
@@ -81,8 +82,7 @@ def print_table(list):
         ga = i["goals_against"]
         gd = int(gf) - int(ga)
         p = (int(w) * 3) + int(d)
-
-        print(f"| {num} | {nation} | {w} | {d} | {l} | {gf} | {ga} | {gd} | {p} | ")
+        print(f"| {num} |",f" {nation} ".ljust(14),f"| {w} | {d} | {l} | {gf} | {ga} | {gd} | {p} | ")
         num += 1
     print("-" * 30)
 
